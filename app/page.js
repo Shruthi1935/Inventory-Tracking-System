452e2a7ca621112149e5d5661fb83979addc8200
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-07-31 18:08:45 -0500

    set up environment

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '/Users/yshru/InventoryTrackingSystem/Inventory-Tracking-System/app/globals.css'; // Ensure this path matches where your global.css is located
+
+export default function Home() {
+  return (
+    <div className="bg-slate-600 p-4 font-Almeda">Hi world!</div>
+  );
+}
