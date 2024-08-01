6e298e7a08d617b1ad73625e67de8d33060e6ddf
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-08-01 18:03:56 -0500

    added css animation - framer does not work

--- app/page.js
+++ app/page.js
@@ -6,11 +6,22 @@ export default function Home() {
   return (
     <div>
       <section id="intro" className="h-screen w-screen bg-[#FFF8EF] flex items-center justify-center">
-        <div className="flex flex-col space-y-8 items-center justify-center fade-in fade-out"> 
-          <div className="font-custom text-5xl text-[#550A1F] text-center m-3"> WELCOME TO YOUR </div>
-          <Image src={gglogo} alt="Gourmet Grotto Logo" width={500} height={500} />
-          <div className="font-custom text-9xl text-[#550A1F] text-center animate-typing overflow-hidden whitespace-nowrap"> Gourmet Grotto </div>
+
+        <div className="flex-col">
+          <div className="fade-in">
+            <div className="flex flex-col space-y-8 items-center justify-center fade-out"> 
+              <div className="font-custom text-5xl text-[#550A1F] text-center m-3"> WELCOME TO YOUR </div>
+              <Image src={gglogo} alt="Gourmet Grotto Logo" width={500} height={500} />
+              <div className="font-custom text-9xl text-[#550A1F] text-center animate-typing overflow-hidden whitespace-nowrap"> Gourmet Grotto </div>
+            </div>
+          </div>
+          <div className="invisible"> 
+            <div className="fade-in-two">
+              <div className="font-custom text-5xl text-[#550A1F] text-center"> Scroll Down to Access Grotto </div>
+            </div>          
+          </div>
         </div>
+
       </section>
 
       <section id="inventorySystem" className="h-screen w-screen bg-[#300A15]"> 
