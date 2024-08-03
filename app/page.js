0ddbdf4a6c46f5b59077f3fd640946952de4cf13
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-08-03 15:23:04 -0500

    created inventory layout, have to map items

--- app/page.js
+++ app/page.js
@@ -79,34 +79,44 @@ export default function Home() {
     <div>
       <section id="intro" className="h-screen w-screen bg-[#FFF8EF] flex items-center justify-center">
         <div id="welcome-screen" className="fade-in">
-          <div className="flex flex-col space-y-8 items-center justify-center fade-out"> 
-            <div className="font-custom text-5xl text-[#550A1F] text-center m-3"> WELCOME TO YOUR </div>
-            <Image src={ggDessert} alt="Gourmet Grotto Logo" width={500} height={500} />
+          <div className="absolute top-0 left-0 m-4">
+            <Image src={ggLogo} alt="Gourmet Grotto Logo" width={100} height={100} className="text-left"/>
+          </div>
+          <div className="flex flex-col space-y-4 items-center justify-center fade-out"> 
+            <div className="font-custom text-5xl text-[#550A1F] text-center"> WELCOME TO YOUR </div>
+            <Image src={ggDessert} alt="Gourmet Grotto Dessert" width={500} height={500} />
             <div className="font-custom text-9xl text-[#550A1F] text-center animate-typing overflow-hidden whitespace-nowrap"> Gourmet Grotto </div>
           </div>
         </div>
 
-        <div id="access-screen" className="fade-in h-screen w-screen bg-[#300A15] flex items-center justify-center hidden">    
-          <div className="flex flex-col items-center justify-center space-y-12"> 
+        <div id="access-screen" className="fade-in h-screen w-screen bg-[#300A15] flex items-center justify-center hidden">  
+          <div className="flex flex-col items-center justify-center space-y-14"> 
             <span className="font-custom text-7xl text-[#FFF8EF] text-center animate-typing overflow-hidden whitespace-nowrap"> Access Your Inventory Here </span>
-            <button className="fade-in-after font-custom text-xl text-[#300A15] text-center bg-[#FFF8EF] rounded-full py-2 px-5 hover:-translate-y-3 duration-500"> Open Grotto </button>
+            <button className="fade-in-after font-custom text-xl text-[#FFF8EF] text-center border border-[#FFF8EF] rounded-full py-2 px-5 hover:-translate-y-3 duration-500"> Open Grotto </button>
           </div>
         </div>
       </section>
 
 
       <section id="inventory" className="h-screen w-screen bg-[#300A15]"> 
-        <div className="flex flex-col space-y-6"> 
-          <div className="font-custom text-8xl text-[#FFF8EF] text-left"> Inventory </div>
-          <div className="flex flex-row space-x-6">
-            <div className="grid grid-col-2 gap-4 text-[#FFF8EF]">
-              have to make it a grid and map it
-            </div>
-            <div className="flex flex-col space-y-4">
-              <div>search/add</div>
-              <div>display queries</div>
+        <div className="p-16">
+          
+          <div className="flex flex-col space-y-5"> 
+            <div className="font-custom text-8xl text-[#FFF8EF]"> Inventory </div>
+
+            <div className="flex flex-row space-x-8"> 
+              <div className="grid bg-[#FFF8EF] p-8 rounded-sm "> 
+                <div> hii </div> 
+                <div> hii </div> 
+              </div>
+
+              <div className="flex flex-col space-y-4"> 
+                <input type="text" placeholder="search / add..." className="font-custom text-lg text-center text-[#FFF8EF] bg-[#7C4455] rounded-full py-2 px-32" />
+                <div className="flex bg-[#FFF8EF] p-5 rounded-sm"> results </div>
+              </div>
             </div>
           </div>
+
         </div>
       </section>
 
