9728c68fda7154d83e6d381e6aacba6118a72867
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-08-04 23:29:55 -0500

    added key props

--- app/inventory/page.js
+++ app/inventory/page.js
@@ -1,26 +1,22 @@
 "use client"
-import React from 'react';
-import { useEffect, onClick, useState, onChange } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import { firestore } from "@/firebase"
-import { query, collection, getDocs} from "firebase/firestore";
+import { query, collection, getDocs } from "firebase/firestore";
 import { doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
-
 
 export default function Inventory() {
 
   // Inventory Management Helper Functions
   const [inventory, setInventory] = useState([]); // default value is empty array
-  const [open, setOpen] = useState(false); // default value is false
   const [itemName, setItemName] = useState(''); // item name to find or add items
   const [searchResults, setSearchResults] = useState([]); // search to find or add items
   const [successMessage, setSuccessMessage] = useState('');
   const [deleteMessage, setDeleteMessage] = useState('');
 
-
-  // fetch inventory from firestone
+  // Fetch inventory from Firestore
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'));
     const docs = await getDocs(snapshot);
@@ -73,7 +69,7 @@ export default function Inventory() {
       }
     }
     await updateInventory();
-    setDeleteMessage(`Successfullt deleted from inventory.`);
+    setDeleteMessage(`Successfully deleted from inventory.`);
     setTimeout(() => setDeleteMessage(''), 2000);
   };
 
@@ -91,94 +87,89 @@ export default function Inventory() {
     updateInventory();
   }, []);
 
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-
   return (
     <div>
-    <section id="inventory" className="h-screen w-screen bg-[#300A15] fade-in"> 
+      <section id="inventory" className="h-screen w-screen bg-[#300A15] fade-in"> 
 
-      <div className="py-10 px-24">
-        <div className="absolute top-0 left-0 right-0 mt-5"> 
-          {/* Confirmation message */}
-          {successMessage && (
-            <div className="fade-in fade-out-two font-custom text-[#e1e1e1db] text-center mb-4">
-              {successMessage}
-            </div>
-          )}
-        </div>
-        <div className="absolute top-0 left-0 right-0 mt-5"> 
-          {/* Confirmation message */}
-          {deleteMessage && (
-            <div className="fade-in fade-out-two font-custom text-[#e1e1e1db] text-center mb-4">
-              {deleteMessage}
-            </div>
-          )}
-        </div>
+        <div className="py-10 px-24">
+          <div className="absolute top-0 left-0 right-0 mt-5"> 
+            {/* Confirmation message */}
+            {successMessage && (
+              <div className="fade-in fade-out-two font-custom text-[#e1e1e1db] text-center mb-4">
+                {successMessage}
+              </div>
+            )}
+          </div>
+          <div className="absolute top-0 left-0 right-0 mt-5"> 
+            {/* Delete message */}
+            {deleteMessage && (
+              <div className="fade-in fade-out-two font-custom text-[#e1e1e1db] text-center mb-4">
+                {deleteMessage}
+              </div>
+            )}
+          </div>
 
-      <div className="flex flex-col space-y-12 font-custom text-md"> 
-        <div className="font-custom text-8xl text-[#e1e1e1db]"> INVENTORY </div>
+          <div className="flex flex-col space-y-12 font-custom text-md"> 
+            <div className="font-custom text-8xl text-[#e1e1e1db]"> INVENTORY </div>
 
-        <div className="flex flex-row space-x-12"> 
-          <div className="flex flex-col bg-[#7c445566] p-10 rounded-md overflow-hidden overflow-y-auto max-h-[600px] w-full"> 
-            <div className="flex flex-row p-2 mb-5">
-              <h1 className="text-[#e1e1e1db] w-3/4 text-2xl font-bold">Item</h1>
-              <h1 className="text-[#e1e1e1db] w-1/4 text-2xl font-bold text-center">Quantity</h1>
-            </div>
-            {inventory.map(item => (
-              <div className="flex flex-row justify-between border-b border-[#9e8f933d] p-2">
-                <div className="text-[#e1e1e1db] flex-shrink-0 w-3/4"> {item.name} </div>
-                <div className="flex flex-row items-center justify-between space-x-8 w-1/4">
+            <div className="flex flex-row space-x-12"> 
+              <div className="flex flex-col bg-[#7c445566] p-10 rounded-md overflow-hidden overflow-y-auto max-h-[600px] w-full"> 
+                <div className="flex flex-row p-2 mb-5">
+                  <h1 className="text-[#e1e1e1db] w-3/4 text-2xl font-bold">Item</h1>
+                  <h1 className="text-[#e1e1e1db] w-1/4 text-2xl font-bold text-center">Quantity</h1>
+                </div>
+                {inventory.map(item => (
+                  <div key={item.name} className="flex flex-row justify-between border-b border-[#9e8f933d] p-2">
+                    <div className="text-[#e1e1e1db] flex-shrink-0 w-3/4"> {item.name} </div>
+                    <div className="flex flex-row items-center justify-between space-x-8 w-1/4">
+                      <button 
+                        onClick={() => removeItem(item.name)}
+                        className="text-[#e1e1e1db]"> <FontAwesomeIcon icon={faMinus} />
+                      </button>
+                      <div className="text-[#e1e1e1db]"> {item.quantity} </div>
+                      <button 
+                        onClick={() => addItem(item.name)}
+                        className="text-[#e1e1e1db]"> <FontAwesomeIcon icon={faPlus} />
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="flex flex-col space-y-8 w-full"> 
+                <div className="flex flex-row space-x-4">
+                  <input 
+                    type="text" 
+                    value={itemName}
+                    onChange={handleSearch}
+                    placeholder="search / add inventory ..." 
+                    className="font-custom text-lg text-center text-[#e1e1e1db] bg-[#7C4455] rounded-full py-2 px-28 w-full" 
+                  />
                   <button 
-                    onClick={() => removeItem(item.name)}
-                    className="text-[#e1e1e1db]"> <FontAwesomeIcon icon={faMinus} />
+                    onClick={() => addItem(itemName)}
+                    className="text-md text-[#e1e1e1db] border border-[#e1e1e1db] rounded-full py-1 px-6 hover:-translate-y-1 duration-200"> Add
                   </button>
-                  <div className="text-[#e1e1e1db]"> {item.quantity} </div>
-                  <button 
-                    onClick={() => addItem(item.name)}
-                    className="text-[#e1e1e1db]"> <FontAwesomeIcon icon={faPlus} />
-                  </button>
+                </div>
+
+                <div className="flex-col bg-[#7c445566] p-10 rounded-md overflow-hidden overflow-y-auto h-[525px] max-h-[525px] w-full"> 
+                  {searchResults.map(item => (
+                    <div key={item.name} className="flex flex-row items-center justify-between border-b border-[#9e8f933d] p-2">
+                      <div className="text-[#e1e1e1db] flex-shrink-0 w-3/4"> {item.name} </div>
+                      <div className="flex flex-row items-center justify-between space-x-8 w-1/4">
+                        <div className="text-[#e1e1e1db]"> {item.quantity} </div>
+                        <button 
+                          onClick={() => deleteItem(item.name)}
+                          className="text-sm text-[#e1e1e1db] border border-[#e1e1e1db] rounded-full py-1 px-3 hover:-translate-y-1 duration-200"> Delete
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            ))}
-          </div>
-
-          <div className="flex flex-col space-y-8 w-full"> 
-            <div className="flex flex-row space-x-4">
-              <input 
-                type="text" 
-                value={itemName}
-                onChange={handleSearch}
-                placeholder="search / add inventory ..." 
-                className="font-custom text-lg text-center text-[#e1e1e1db] bg-[#7C4455] rounded-full py-2 px-28 w-full" 
-              />
-              <button 
-                onClick={() => addItem(itemName)}
-                className="text-md text-[#e1e1e1db] border border-[#e1e1e1db] rounded-full py-1 px-6 hover:-translate-y-1 duration-200"> Add
-              </button>
-            </div>
-
-            <div className="flex-col bg-[#7c445566] p-10 rounded-md overflow-hidden overflow-y-auto h-[525px] max-h-[525px] w-full"> 
-              {searchResults.map(item => (
-                <div className="flex flex-row items-center justify-between border-b border-[#9e8f933d] p-2">
-                  <div className="text-[#e1e1e1db] flex-shrink-0 w-3/4"> {item.name} </div>
-                  <div className="flex flex-row items-center justify-between space-x-8 w-1/4">
-                    <div className="text-[#e1e1e1db]"> {item.quantity} </div>
-                    <button 
-                      onClick={() => deleteItem(item.name)}
-                      className="text-sm text-[#e1e1e1db] border border-[#e1e1e1db] rounded-full py-1 px-3 hover:-translate-y-1 duration-200"> Delete
-                    </button>
-                  </div>
-                </div>
-              ))}
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
-
-  </section>
-  </div>
   );
 }
